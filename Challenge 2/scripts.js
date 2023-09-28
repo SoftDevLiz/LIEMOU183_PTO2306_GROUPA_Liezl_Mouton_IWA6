1e36d970d06d146657786df66f7ddb4551d9321d
Liezl Mouton <liz.mouton1@gmail.com>
2023-09-28 17:48:07 +0200

    Challenge 2 complete

--- Challenge 2/scripts.js
+++ Challenge 2/scripts.js
@@ -1,5 +1,5 @@
 const rent = 400;
-const tax = '8%';
+const tax = "8%";
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
@@ -8,10 +8,21 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+let taxAsDecimal;
+let startingAfterTax;
+let balance;
+
+let taxAsInt = parseInt(tax);
+
+if (
+  typeof hourOfDay &&
+  typeof minuteOfDay !== undefined &&
+  hourOfDay === 00 &&
+  minuteOfDay === 00
+) {
+  taxAsDecimal = taxAsInt / 100;
+  taxWorkedOut = taxAsDecimal * salary;
+  startingAfterTax = salary - taxWorkedOut;
+  balance = startingAfterTax - transport - food - rent;
 }
-	
-console.log(balance.toFixed(3))
+console.log(balance.toFixed(2));
